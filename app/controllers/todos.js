98dac88589e6f636560a059635092027142048fe
Tyler Martinez <slightlytyler@gmail.com>
2014-10-12 22:46:05 -0500

    Added todos controller

---
+++ app/controllers/todos.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  actions: {
+    createTodo: function() {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
